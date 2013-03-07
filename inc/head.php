8a2a3cde9302437dba97893b7efd55a76331056b
Robert Manukyan <manukyanrobert@gmail.com>
2013-03-07 10:21:46 -0800

    php

---
+++ inc/head.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+    
+    <!--
+
+        made by tetragram
+        www.madebytetragram.com
+        hello@madebytetragram.com
+
+    -->
+    
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+	<title>Untitled</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/grid.css">
+
+	<!-- Scripts -->
+	<script type="text/javascript" src="js/jquery.1.8.0.min.js"></script>
+
+
+    <!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+</head>
+<body>
+
+
+
